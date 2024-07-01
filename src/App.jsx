3f60686d045a6908287ec7894fe38d3761a584ad
Oluwabanke Eboda <abeegarl@gmail.com>
2024-07-01 17:31:26 +0100

    feat:impplement van pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import About from './pages/About.jsx'
-import Home from "./pages/Home.jsx";
+import About from './pages/Vans/About.jsx'
+import Home from "./pages/Vans/Home.jsx";
+import "./server"
+import Vans from "./pages/Vans/Vans.jsx";
+import VanDetail from "./pages/Vans/VanDetail.jsx";
+import Layout from "./components/Layout.jsx";
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
 
 function App() {
     return (
         <BrowserRouter>
-            <header>
-                <Link className="site-logo" to="/">#VanLife</Link>
-                <nav>
-                <Link to="/about">About</Link>
-                </nav>
-            </header>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/vans" element={<Vans />} />
+                    <Route path="/vans/:id" element={<VanDetail />} />
+                    <Route path="/host" element={<Dashboard />} />
+                    <Route path="/host/income" element={<Income />} />
+                    <Route path="/host/reviews" element={<Reviews />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
